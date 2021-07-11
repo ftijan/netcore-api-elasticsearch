576d4c3235872af26bc4f9237cd47056bd05f910
Filip Tijan <ftijan@gmail.com>
2021-07-11 18:03:09 +0100

    adding solution files

---
+++ src/Example.Elasticsearch.Api/Extensions/ModelConfig.cs
@@ -0,0 +1,22 @@
+﻿using Example.Elasticsearch.Api.Models;
+using Nest;
+using System;
+
+namespace Example.Elasticsearch.Api.Extensions
+{
+    public static class ModelConfig
+    {       
+        public static ConnectionSettings WithModelConfig(this ConnectionSettings connectionSettings)
+        {
+            if (connectionSettings is null)
+            {
+                throw new ArgumentNullException(nameof(connectionSettings));
+            }
+
+            // Employee has a property with a name Id and Nest will attempt to
+            // map it to internal _id field - disable it and allow Elasticsearch to
+            // generate its own _id value
+            return connectionSettings.DefaultMappingFor<Employee>(x => x.DisableIdInference());
+        }
+    }
+}
---
+++ src/Example.Elasticsearch.Api/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Example.Elasticsearch.Api.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
